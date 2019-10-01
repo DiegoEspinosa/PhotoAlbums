12c38c027273552c80361e439be5da88958e3110
despinosa <despinosa@inmotionsoftware.com>
2019-10-01 16:16:10 -0500

    Initial Commit

---
+++ PhotoAlbums/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoAlbums
+//
+//  Created by Diego Espinosa on 10/1/19.
+//  Copyright © 2019 Diego Espinosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
