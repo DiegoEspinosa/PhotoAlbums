bb9a273bdedde604f8cf8cd7630c8773f7824a90
despinosa <despinosa@inmotionsoftware.com>
2019-10-02 13:19:37 -0500

    Modified collection view data source functions to return valid data

--- PhotoAlbums/Controller/PhotoAlbumsCollectionViewController.swift
+++ PhotoAlbums/Controller/PhotoAlbumsCollectionViewController.swift
@@ -31,14 +31,12 @@ class PhotoAlbumsCollectionViewController: UICollectionViewController {
     // MARK: - UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
+        return photoAlbums.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
