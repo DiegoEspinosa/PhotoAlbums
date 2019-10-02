434b60352eb1c53e84c209570000ef4c04120389
despinosa <despinosa@inmotionsoftware.com>
2019-10-02 16:17:55 -0500

    Setup AlbumPhotoCollectionViewCell.swift/xib

--- PhotoAlbums/Controller/Cells/AlbumPhotosCollectionViewCell.swift
+++ PhotoAlbums/Controller/Cells/AlbumPhotosCollectionViewCell.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class AlbumPhotosCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var photoImageView: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
