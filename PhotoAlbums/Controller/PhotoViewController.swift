31240f9801fbd583a8245c5614bac379dd8d1a69
despinosa <despinosa@inmotionsoftware.com>
2019-10-05 18:46:53 -0500

    Added functionality to save images to photo library

--- PhotoAlbums/Controller/PhotoViewController.swift
+++ PhotoAlbums/Controller/PhotoViewController.swift
@@ -8,6 +8,9 @@
 //  Found solution for imageTapped on Stackoverflow:
 //  https://stackoverflow.com/questions/34694377/swift-how-can-i-make-an-image-full-screen-when-clicked-and-then-original-size
 //
+//  Found solution for saving images
+//  https://www.hackingwithswift.com/read/13/5/saving-to-the-ios-photo-library
+//
 
 import UIKit
 
@@ -39,6 +42,18 @@ class PhotoViewController: UIViewController {
         }
     }
     
+    @objc func saveImage(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            let alertController = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alertController, animated: true)
+        } else {
+            let alertController = UIAlertController(title: "Saved", message: "You succesfully saved the image", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alertController, animated: true)
+        }
+    }
+    
     @objc private func dismissFullscreenImage(_ sender: UITapGestureRecognizer) {
         self.navigationController?.isNavigationBarHidden = false
         sender.view?.removeFromSuperview()
@@ -58,6 +73,10 @@ class PhotoViewController: UIViewController {
         self.navigationController?.isNavigationBarHidden = true
     }
 
-
+    @IBAction func saveImageToPhotoLibrary(_ sender: Any) {
+        guard let image = photoImageView.image else {return}
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveImage), nil)
+    }
+    
 }
 
